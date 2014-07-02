cc91c802f2e55c722abfb0fa4f9867d738ba9ea6
HariPriyankaAkula <haripriyanka.akula@gmail.com>
2014-07-02 16:26:03 +0530

    Create RecordsCell.m

---
+++ RecordsCell.m
@@ -0,0 +1,29 @@
+//
+//  RecordsCell.m
+//  CoreDataPOC
+//
+//  Created by Hari Priyanka on 02/07/14.
+//  Copyright (c) 2014. All rights reserved.
+//
+
+#import "RecordsCell.h"
+
+@implementation RecordsCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
